316928c2abdcd9dd7c5360374a4933698c78ac5f
Mariusz Augustyniak <maugustyniak@gmail.com>
2025-01-21 10:33:48 +0100

    No sign up

--- app/(auth-pages)/sign-in/page.tsx
+++ app/(auth-pages)/sign-in/page.tsx
@@ -9,22 +9,22 @@ import Logo from '@/components/logo';
 export default async function Login(props: { searchParams: Promise<Message> }) {
   const searchParams = await props.searchParams;
   return (
-    <div className="flex flex-col gap-16 items-center">
-      <div className="flex gap-8 justify-center items-center">
+    <div className="flex flex-col gap-4 items-center">
+      <div className="flex gap-4 justify-center items-center">
         <a
           href="/"
           target="_blank"
           rel="noreferrer"
         >
-          <Logo className="h-64 w-64" />
+          <Logo className="h-52 w-52" />
         </a>
       </div>
       <form className="flex-1 flex flex-col min-w-64">
         <h1 className="text-2xl font-medium">Sign in</h1>
         <p className="text-sm text-foreground">
           Don't have an account?{' '}
-          <Link className="text-foreground font-medium underline" href="/sign-up">
-            Sign up
+          <Link className="text-foreground font-medium underline" href="/">
+            Contact with administrator.
           </Link>
         </p>
         <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
